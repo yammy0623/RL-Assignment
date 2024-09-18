81aaf131547d2e498fd0c3d34e1f4a845e69322a
Christine Hsieh(謝子涔) <Christine_Hsieh@asus.com>
2024-09-18 19:05:35 +0800

    20240918

--- Assignment1/DP_solver.py
+++ Assignment1/DP_solver.py
@@ -17,7 +17,7 @@ class DynamicProgramming:
         self.discount_factor = discount_factor
         self.threshold = 1e-4  # default threshold for convergence
         self.values = np.zeros(grid_world.get_state_space())  # V(s)
-        self.policy = np.zeros(grid_world.get_state_space(), dtype=int)  # pi(s)
+        self.policy = np.zeros(grid_world.get_state_space(), dtype=int)  # pi(s) 22
 
     def set_threshold(self, threshold: float) -> None:
         """Set the threshold for convergence
@@ -54,6 +54,14 @@ class DynamicProgramming:
             float
         """
         # TODO: Get reward from the environment and calculate the q-value
+        # q_value = 0.0 
+        next_state, reward, done = self.grid_world.step(state, action)
+        # if done: # 走到終點
+        if next_state != state: # 無路可走
+            get_reward = reward + self.discount_factor * self.values[next_state]
+        else:
+            get_reward = reward
+        return get_reward
         raise NotImplementedError
 
 
@@ -81,16 +89,34 @@ class IterativePolicyEvaluation(DynamicProgramming):
             float: value
         """
         # TODO: Get the value for a state by calculating the q-values
-        raise NotImplementedError
+        # policy: [22, 4]
+        value = 0.0
+        num_actions = self.grid_world.get_action_space()
+        for action in range(num_actions):
+            value += self.policy[state, action] * self.get_q_value(state, action)
+        return value
+        # raise NotImplementedError
 
     def evaluate(self):
         """Evaluate the policy and update the values for one iteration"""
         # TODO: Implement the policy evaluation step
+        
+        theta = 0.0
+        while True:
+            delta = 0
+            for s in range(self.grid_world.get_state_space()):
+                v = self.values[s]
+                
+                self.values[s] = self.get_state_value(s)
+                delta = max(delta, abs(v - self.values[s]))
+            if delta < theta:
+                break            
         raise NotImplementedError
 
     def run(self) -> None:
         """Run the algorithm until convergence."""
         # TODO: Implement the iterative policy evaluation algorithm until convergence
+        self.evaluate()
         raise NotImplementedError
 
 
